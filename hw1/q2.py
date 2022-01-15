9a86666ce46519543cfd291c4505a7284da22aa3
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-01-14 21:48:38 -0800

    passing several test including bounds!

--- hw1/q2.py
+++ hw1/q2.py
@@ -1,13 +1,13 @@
-# 2D list with each peg as a list
-hanoi_board = None
+### GLOBALS
+board = None
 last_p = 0
 num_moves = 0
 
 # used for ensuring legal moves
 def check_board():
-    global hanoi_board
-    for i in range(len(hanoi_board)):
-        p = hanoi_board[i]
+    global board
+    for i in range(len(board)):
+        p = board[i]
         tmp = p.copy()
         tmp.sort()
         tmp.reverse()
@@ -16,62 +16,72 @@ def check_board():
             return False
     return True
 
-# move block from peg p -> q sequentially
+# move block from peg p -> q 1 peg at a time
 def move(p, q):
     global num_moves
-    global hanoi_board
+    global board
 
     for i in range(abs(p-q)):
         num_moves += 1
 
         # moving block to the right
         if p < q:
-            b = hanoi_board[p+i].pop()
-            hanoi_board[p+i+1].append(b)
+            b = board[p+i].pop()
+            board[p+i+1].append(b)
         else:
-            b = hanoi_board[p-i].pop()
-            hanoi_board[p-i-1].append(b)
+            b = board[p-i].pop()
+            board[p-i-1].append(b)
 
-        print(f"After move {num_moves}: ", hanoi_board)
-        assert check_board(), "ILLEGAL MOVE YA MONKEY!"
+        print(f"After move {num_moves}: ", board)
+        assert check_board(), "ILLEGAL MOVE!"
 
-def hanoi_liberty(n, p=0, a=None, mode=1):
+def hanoi_liberty(n, p=0, a=0, mode=1):
     # n: number of blocks
-    # p: peg number
+    # p: peg index
+        # mode 1: move this peg
+        # mode 2: move to this peg
     # a: number of adj moves to make
-    global last_p, hanoi_board
-       
-    # can this be removed?
-    if a == None:
-        a = n-1
+    # mode: 1.unravel, 2.reverse, 3.restack
 
-    # can be simplified
-    if a <= 0 or p < 0:
+    global last_p, board
+
+    if a <= 0:
         mode += 1
     
     if mode == 1:
-        # print("unravel mode")
+        # print("mode 1: unravel")
         move(p, p+a)
         return hanoi_liberty(n, p, a-1)
     elif mode == 2:
-        print("reverse mode")
+        # print("mode 2: reverse")
         for i in range(n, 1, -1):
             # shuffle decaying group to the right
             for j in range(1, i+1):
                 s = last_p - j
                 move(s, s+1)
-            # last peg's block to the first unoccupide on the left
+            # last peg's block to the first unoccupided on the left
             move(last_p, last_p-i)
         return hanoi_liberty(n, n-2, 0, mode)
     elif mode == 3:
-        print("restack mode")
+        # print("mode 3: restack")
         for i in range(-1, n-1):
             move(p-i, n)
 
+# test boards!
+boards = []
+boards.append([[]])
+boards.append([[1],[]])
+boards.append([[2,1],[],[]])
+boards.append([[3,2,1],[],[],[]])
+boards.append([[4,3,2,1],[],[],[],[]])
+boards.append([[5,4,3,2,1],[],[],[],[],[]])
+boards.append([[6,5,4,3,2,1],[],[],[],[],[],[]])
+boards.reverse()
 
-hanoi_board = [[3,2,1],[],[],[]]
-last_p = len(hanoi_board) - 1
-n = len(hanoi_board[0])
-print("n: ", n)
-hanoi_liberty(n)
-print("number of moves: ", num_moves)
+for board in boards:
+    num_moves = 0
+    last_p = len(board) - 1
+    n = len(board[0])
+    hanoi_liberty(n, a=n-1)
+    print(f"number of moves for n={n}: ", num_moves, "\n")
+    assert num_moves >= 3 * n^2 or not n, "bounds reported incorrectly!"
