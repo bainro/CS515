48beebc7c46a43dc6d86834b078dcbc4965660ce
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-01-20 00:15:53 -0800

    Add files via upload

--- hw2/q1.py
+++ hw2/q1.py
@@ -10,13 +10,27 @@ def fast_LIS(a):
     a.append(float('-inf'))
     
     # create 2d array of 0s
-    memo = [[0]*n]*n
+    memo = [[0]*(n+1)]*n
 
     # iterate in reverse dir
     for j in range(n-1, -1, -1):
         # normal forward dir
-        for i in range(j-1):
-            keep = 1 + memo[j, j+1]
-            skip = memo[i, j+1]
+        for i in range(-1, j-1):
+            keep = 1 + memo[j][j+1]
+            skip = memo[i][j+1]
+            # print(a[i], a[j])
             if a[i] >= a[j]:
-                
+                # print("skip")
+                memo[i][j] = skip
+            else:
+                print("keep: ", keep)
+                print("skip: ", skip, "\n")
+                memo[i][j] = max(keep, skip)
+
+    print("memo: ", memo)
+    return memo[0][1]
+
+test_a = [0, 1, 2]
+result = fast_LIS(test_a)
+print("test_a", test_a)
+print("result: ", result)
