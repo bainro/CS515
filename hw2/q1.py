0d396055cb16fb109256fa33f91f5515e7b330b4
Robert Bain <31903812+bainro@users.noreply.github.com>
2022-01-19 23:42:22 -0800

    Add files via upload

--- hw2/q1.py
+++ hw2/q1.py
@@ -3,4 +3,20 @@
 Returns length of longest subsequence that's 
 both increasing and alternating wrt parity.
 '''
+def fast_LIS(a):
+    n = len(a)
 
+    # add sentinel to the end
+    a.append(float('-inf'))
+    
+    # create 2d array of 0s
+    memo = [[0]*n]*n
+
+    # iterate in reverse dir
+    for j in range(n-1, -1, -1):
+        # normal forward dir
+        for i in range(j-1):
+            keep = 1 + memo[j, j+1]
+            skip = memo[i, j+1]
+            if a[i] >= a[j]:
+                
